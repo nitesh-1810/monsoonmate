57163d38f062de7903df55a474f4b917f445e1f3
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-09-17 22:41:56 +0530

    Update in API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "9c526a18dde750571bbc7e30888f3c2b";
-const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeather = async (city) => {
   try {
